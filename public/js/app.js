47d504e95ce44c55412756280af5771d24f719ae
Daniel <daniel@nodocomun.org>
2025-10-06 17:17:59 +0200

    Revert to simple dot symbol for comment bubbles

--- public/js/app.js
+++ public/js/app.js
@@ -407,6 +407,7 @@ function drawLink(fromElement, toElement, linkId, colorIndex = 0, bubbleOffset =
 
     const bubble = document.createElement('div');
     bubble.className = 'comment-bubble';
+    bubble.textContent = '●';
     bubble.style.left = bubbleX + 'px';
     bubble.style.top = bubbleY + 'px';
     bubble.style.borderColor = color;
